5b84b3da06aae6c7d1f9ed92538daf372803a770
linnefromice <linnefromice@gmail.com>
2022-11-24 23:01:06 +0900

    update: deposit

--- x/handson/keeper/msg_server_deposit.go
+++ x/handson/keeper/msg_server_deposit.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/linnefromice/handson/x/handson/types"
@@ -9,9 +10,22 @@ import (
 
 func (k msgServer) Deposit(goCtx context.Context, msg *types.MsgDeposit) (*types.MsgDepositResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	sender, _ := sdk.AccAddressFromBech32(msg.Creator)
 
-	// TODO: Handling the message
-	_ = ctx
+	err := k.bankKeeper.SendCoinsFromAccountToModule(ctx, sender, types.ModuleName, sdk.NewCoins(msg.Amount))
+	if err != nil {
+		return &types.MsgDepositResponse{}, err
+	}
+
+	lpCoin := sdk.NewCoin(fmt.Sprintf("share-%s", msg.Amount.Denom), msg.Amount.Amount)
+	err = k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.NewCoins(lpCoin))
+	if err != nil {
+		return &types.MsgDepositResponse{}, err
+	}
+	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sender, sdk.NewCoins(lpCoin))
+	if err != nil {
+		return &types.MsgDepositResponse{}, err
+	}
 
 	return &types.MsgDepositResponse{}, nil
 }
