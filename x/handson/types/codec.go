7f062f5bccba3540f6b47b133f42d89dd7b9e46f
linnefromice <linnefromice@gmail.com>
2022-11-24 23:08:36 +0900

    exec 'ignite scaffold message withdraw amount:coin'

--- x/handson/types/codec.go
+++ x/handson/types/codec.go
@@ -9,12 +9,16 @@ import (
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgDeposit{}, "handson/Deposit", nil)
+	cdc.RegisterConcrete(&MsgWithdraw{}, "handson/Withdraw", nil)
 	// this line is used by starport scaffolding # 2
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgDeposit{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgWithdraw{},
 	)
 	// this line is used by starport scaffolding # 3
 
