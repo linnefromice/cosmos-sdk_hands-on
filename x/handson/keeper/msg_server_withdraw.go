1879d0d1cd4f94d688a1fc2453ed97f1d06fd16d
linnefromice <linnefromice@gmail.com>
2022-11-24 23:15:24 +0900

    update: withdraw

--- x/handson/keeper/msg_server_withdraw.go
+++ x/handson/keeper/msg_server_withdraw.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/linnefromice/handson/x/handson/types"
@@ -9,9 +10,22 @@ import (
 
 func (k msgServer) Withdraw(goCtx context.Context, msg *types.MsgWithdraw) (*types.MsgWithdrawResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	sender, _ := sdk.AccAddressFromBech32(msg.Creator)
 
-	// TODO: Handling the message
-	_ = ctx
+	lpCoin := sdk.NewCoin(fmt.Sprintf("share-%s", msg.Amount.Denom), msg.Amount.Amount)
+	err := k.bankKeeper.SendCoinsFromAccountToModule(ctx, sender, types.ModuleName, sdk.NewCoins(lpCoin))
+	if err != nil {
+		return &types.MsgWithdrawResponse{}, err
+	}
+	err = k.bankKeeper.BurnCoins(ctx, types.ModuleName, sdk.NewCoins(lpCoin))
+	if err != nil {
+		return &types.MsgWithdrawResponse{}, err
+	}
+
+	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sender, sdk.NewCoins(msg.Amount))
+	if err != nil {
+		return &types.MsgWithdrawResponse{}, err
+	}
 
 	return &types.MsgWithdrawResponse{}, nil
 }
